d25d111fae621134057b599a039895f9da5de73b
tommasocarzaniga <tommaso.carzaniga@gmail.com>
2026-01-20 15:35:48 +0100

    Update run_one.py

--- scripts/run_one.py
+++ scripts/run_one.py
@@ -1,1 +1,13 @@
+from iaea_project.utils import ensure_dirs, RAW_DIR
+from iaea_project.cleaning import clean_cyclotron_df
+import pandas as pd
 
+def main():
+    ensure_dirs()
+    raw_csv = RAW_DIR / "iaea_cyclotrons_raw.csv"
+    df = pd.read_csv(raw_csv)
+    df2 = clean_cyclotron_df(df)
+    print(df2.head())
+
+if __name__ == "__main__":
+    main()
