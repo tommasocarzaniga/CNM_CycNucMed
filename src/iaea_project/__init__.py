a74560c86690b3a5b6ee165981273467864c6e89
tommasocarzaniga <tommaso.carzaniga@gmail.com>
2026-01-20 21:07:57 +0100

    Update __init__.py

--- src/iaea_project/__init__.py
+++ src/iaea_project/__init__.py
@@ -1,13 +1,10 @@
 """IAEA cyclotron list project (scrape -> clean -> analyze -> plot -> PDF)."""
 
-from .run import run
-
 __all__ = [
     "scraper",
     "cleaning",
     "analysis",
     "plotting",
     "pdf_report",
-    "utils",
-    "run",
+    "utils"
 ]
