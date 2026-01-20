0f52f42e467ce1124093dabc9cadfc76725aa4cd
tommasocarzaniga <tommaso.carzaniga@gmail.com>
2026-01-20 16:00:42 +0100

    Update __init__.py

--- src/iaea_project/__init__.py
+++ src/iaea_project/__init__.py
@@ -1,1 +1,10 @@
+"""IAEA cyclotron list project (scrape -> clean -> analyze -> plot -> PDF)."""
 
+__all__ = [
+    "scraper",
+    "cleaning",
+    "analysis",
+    "plotting",
+    "pdf_report",
+    "utils",
+]
